aac71b59bb6a8f841d3b7d4332d776048bdf1d64
danielr0101 <daniel.cba.103@gmail.com>
2024-06-17 19:54:09 -0300

    Hero

--- Config.py
+++ Config.py
@@ -8,7 +8,14 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 SPRITESHEET_PATH = os.path.join(BASE_DIR, 'Assets', 'SpriteSheets', 'sheets')
 BACKGROUND_IMAGE = os.path.join(SPRITESHEET_PATH, 'Background', 'Background.png')
 BEE_IMAGE = os.path.join(SPRITESHEET_PATH, 'Mob', 'Small Bee', 'Fly', 'Fly-Sheet.png')
+IDLE_SPRITE = os.path.join(SPRITESHEET_PATH, 'Character', 'Idle', 'Idle-Sheet.png')
+ATTACK_SPRITE = os.path.join(SPRITESHEET_PATH, 'Character', 'Attack-01', 'Attack-01-Sheet.png')
+RUN_SPRITE = os.path.join(SPRITESHEET_PATH, 'Character', 'Run', 'Run-Sheet.png')
 
 SPEED_BEE = 2
 
 ANIMSPEED_BEE = 0.2
+
+ANIMSPEED_HERO_DEFAULT = 0.25
+ANIMSPEED_HERO_IDLE = 0.1
+SPEED_HERO = 4
